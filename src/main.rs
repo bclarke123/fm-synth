c628f66be34845e9b627bd83dc08a011c9ae1add
Ben Clarke <ben@arrayofstars.com>
2023-01-06 16:28:41 -0500

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,12 @@ use fm_synth::octave;
 
 struct Note {
     freq: Option<f64>,
-    duration: u64
+    duration: u64,
 }
 
 impl Note {
     fn new(freq: Option<f64>, duration: u64) -> Self {
-        Self {
-            freq, duration
-        }
+        Self { freq, duration }
     }
     fn some(freq: octave::Note, octave: i32, duration: u64) -> Self {
         Self::new(Some(octave::freq(freq, octave)), duration)
@@ -36,25 +34,69 @@ fn song() -> Vec<Note> {
         .map(|l| l.split(' ').collect::<Vec<_>>())
         .map(|x| match x[0] {
             "S" => Note::silence(x[1].parse::<u64>().unwrap()),
-            "A" => Note::some(octave::A, x[1].parse::<i32>().unwrap(), x[2].parse::<u64>().unwrap()),
-            "B" => Note::some(octave::B, x[1].parse::<i32>().unwrap(), x[2].parse::<u64>().unwrap()),
-            "C" => Note::some(octave::C, x[1].parse::<i32>().unwrap(), x[2].parse::<u64>().unwrap()),
-            "Cs" => Note::some(octave::C_SHARP, x[1].parse::<i32>().unwrap(), x[2].parse::<u64>().unwrap()),
-            "D" => Note::some(octave::D, x[1].parse::<i32>().unwrap(), x[2].parse::<u64>().unwrap()),
-            "Ds" => Note::some(octave::D_SHARP, x[1].parse::<i32>().unwrap(), x[2].parse::<u64>().unwrap()),
-            "E" => Note::some(octave::E, x[1].parse::<i32>().unwrap(), x[2].parse::<u64>().unwrap()),
-            "F" => Note::some(octave::F, x[1].parse::<i32>().unwrap(), x[2].parse::<u64>().unwrap()),
-            "Fs" => Note::some(octave::F_SHARP, x[1].parse::<i32>().unwrap(), x[2].parse::<u64>().unwrap()),
-            "G" => Note::some(octave::G, x[1].parse::<i32>().unwrap(), x[2].parse::<u64>().unwrap()),
-            "Af" => Note::some(octave::A_FLAT, x[1].parse::<i32>().unwrap(), x[2].parse::<u64>().unwrap()),
-            _ => panic!()
+            "A" => Note::some(
+                octave::A,
+                x[1].parse::<i32>().unwrap(),
+                x[2].parse::<u64>().unwrap(),
+            ),
+            "B" => Note::some(
+                octave::B,
+                x[1].parse::<i32>().unwrap(),
+                x[2].parse::<u64>().unwrap(),
+            ),
+            "C" => Note::some(
+                octave::C,
+                x[1].parse::<i32>().unwrap(),
+                x[2].parse::<u64>().unwrap(),
+            ),
+            "Cs" => Note::some(
+                octave::C_SHARP,
+                x[1].parse::<i32>().unwrap(),
+                x[2].parse::<u64>().unwrap(),
+            ),
+            "D" => Note::some(
+                octave::D,
+                x[1].parse::<i32>().unwrap(),
+                x[2].parse::<u64>().unwrap(),
+            ),
+            "Ds" => Note::some(
+                octave::D_SHARP,
+                x[1].parse::<i32>().unwrap(),
+                x[2].parse::<u64>().unwrap(),
+            ),
+            "E" => Note::some(
+                octave::E,
+                x[1].parse::<i32>().unwrap(),
+                x[2].parse::<u64>().unwrap(),
+            ),
+            "F" => Note::some(
+                octave::F,
+                x[1].parse::<i32>().unwrap(),
+                x[2].parse::<u64>().unwrap(),
+            ),
+            "Fs" => Note::some(
+                octave::F_SHARP,
+                x[1].parse::<i32>().unwrap(),
+                x[2].parse::<u64>().unwrap(),
+            ),
+            "G" => Note::some(
+                octave::G,
+                x[1].parse::<i32>().unwrap(),
+                x[2].parse::<u64>().unwrap(),
+            ),
+            "Af" => Note::some(
+                octave::A_FLAT,
+                x[1].parse::<i32>().unwrap(),
+                x[2].parse::<u64>().unwrap(),
+            ),
+            _ => panic!(),
         })
         .collect::<Vec<_>>()
 }
 
 fn set_harmonic(hz: f64, mul: f64) {
     set_carrier_hz(hz);
-    set_modifier_hz(hz * mul);
+    set_modulator_hz(hz * mul);
 }
 
 fn set_carrier_hz(hz: f64) {
@@ -67,26 +109,26 @@ fn set_carrier_silent() {
     *c_lock = None;
 }
 
-fn set_modifier_hz(hz: f64) {
+fn set_modulator_hz(hz: f64) {
     let mut m_lock = unsafe { MODULATOR.lock().unwrap() };
     *m_lock = Some(Box::new(signal::rate(44_100.0).const_hz(hz).sine()));
 }
 
-fn set_modifier_silent() {
+fn set_modulator_silent() {
     let mut m_lock = unsafe { MODULATOR.lock().unwrap() };
     *m_lock = None;
 }
 
 fn run<T: Sample>(device: Device, config: StreamConfig) -> Stream {
-    let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
+    let err_fn = |err| eprintln!("an error occurred on the output audio stream: {err}");
 
     let data_callback = |data: &mut [T], _info: &cpal::OutputCallbackInfo| {
         let mut c_lock = unsafe { CARRIER.lock().unwrap() };
         let mut m_lock = unsafe { MODULATOR.lock().unwrap() };
 
-        if let (Some(carrier), Some(modifier)) = (c_lock.as_deref_mut(), m_lock.as_deref_mut()) {
+        if let (Some(carrier), Some(modulator)) = (c_lock.as_deref_mut(), m_lock.as_deref_mut()) {
             for sample in data.iter_mut() {
-                let frame = (carrier.next() + modifier.next()) as f32;
+                let frame = (carrier.next() + modulator.next()) as f32;
                 *sample = Sample::from(&frame);
             }
         } else {
@@ -130,12 +172,11 @@ fn main() {
     let song = song();
 
     for cur in song {
-
         if let Some(note) = cur.freq {
             set_harmonic(note, harmonic);
         } else {
             set_carrier_silent();
-            set_modifier_silent();
+            set_modulator_silent();
         }
 
         std::thread::sleep(Duration::from_millis(cur.duration));
